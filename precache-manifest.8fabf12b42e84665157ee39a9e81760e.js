77e9d11343ff624d528d0203ba8c397a2b2b6075
RenzoElias <57267514+RenzoElias@users.noreply.github.com>
2020-11-27 12:06:25 -0500

    Updates

---
+++ precache-manifest.8fabf12b42e84665157ee39a9e81760e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4a75d8a9459536ee3a04e8d33ac4cf",
+    "url": "/react-gifsearchapp/index.html"
+  },
+  {
+    "revision": "8082ee4b64aebcca71c7",
+    "url": "/react-gifsearchapp/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/react-gifsearchapp/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gifsearchapp/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8082ee4b64aebcca71c7",
+    "url": "/react-gifsearchapp/static/js/main.69e52b58.chunk.js"
+  },
+  {
+    "revision": "cf8a34755578efd57786",
+    "url": "/react-gifsearchapp/static/js/runtime-main.991ad199.js"
+  }
+]);
